bf52ee6025c56450d92dc0feb228a711a3c25a63
kummari-sujan-kumar <theksujan@gmail.com>
2024-09-05 16:30:38 +0530

    1- project setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongoDB.js';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+const PORT=process.env.PORT || 5000
+
+app.use('/api/auth',authRoutes)
+
+
+
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+})
