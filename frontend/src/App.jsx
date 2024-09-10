dab5b47b5d6633349c3dbf139fcc22e0c0171eb5
kummari-sujan-kumar <theksujan@gmail.com>
2024-09-10 14:47:09 +0530

    3- Auth handeled and routes protected in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,77 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import HomePage from './pages/home/HomePage'
-import LoginPage from './pages/auth/login/LoginPage'
-import SignUpPage from './pages/auth/signup/SignUpPage'
-import Sidebar from './components/common/Sidebar'
-import RightPanel from './components/common/RightPanel'
-import NotificationPage from './pages/notification/NotificationPage'
-import ProfilePage from './pages/profile/ProfilePage'
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { BsFileX } from "react-icons/bs";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 function App() {
+  const {
+    data: authUser,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["authUser"], 
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = res.json();
+        if (!res.ok || data.error){
+          return null;
+        } 
+        console.log("authUser is here: ", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
+
 
   return (
-    <div className='flex max-w-6xl mx-auto'>
-      <Sidebar/>
+    <div className="flex max-w-6xl mx-auto">
+       {authUser && <Sidebar />}
       <Routes>
-          <Route path='/' element={<HomePage/>}></Route>
-          <Route path='/login' element={<LoginPage/>}></Route>
-          <Route path='/signup' element={<SignUpPage/>}></Route>
-          <Route path='/notifications' element={<NotificationPage/>}></Route>
-          <Route path='/profile/:username' element={<ProfilePage/>}></Route>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        ></Route>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        ></Route>
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        ></Route>
       </Routes>
-      <RightPanel/>
+      { authUser && <RightPanel />}
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from'react-router-dom'
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+
+const queryClient=new QueryClient({
+  defaultOptions:{
+    queries:{
+      refetchOnWindowFocus:false,
+      retry:false
+    },
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <App />
+    </QueryClientProvider>
     </BrowserRouter>
+    <Toaster/>
   </StrictMode>,
 )
